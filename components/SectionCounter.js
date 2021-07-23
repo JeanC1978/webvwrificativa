7a3729a81fdc8fa40702e7bebecfd04cc1749929
Juan Silupu <juansilupumaza@gmail.com>
2021-07-23 11:24:25 -0500

    feat: counter finish

--- components/SectionCounter.js
+++ components/SectionCounter.js
@@ -1,27 +1,36 @@
-import React from 'react'
+import React, { useRef } from 'react'
+
+//Components
 import Image from "next/image";
-import CounterImg from "../public/images/counter-img.png";
 import { ButtonBlack } from "./ButtonBlack";
 
-export function SectionCounter(props) {
+//images
+import CounterImg from "../public/images/counter-img.png";
 
+//hooks
+import { useIntersectionObserver } from "../lib/hooks/useIntersectionObserver";
+
+export function SectionCounter(props) {
+    const elementRef = useRef(null);
+    const [inView, entry] = useIntersectionObserver(elementRef, { threshold: 0 });
+    console.log("inView", inView)
 
     return (
-        <div className="section-counter">
+        <div className="section-counter" ref={elementRef}>
             <div className="section-counter__img">
                 <Image src={CounterImg} />
             </div>
             <div className="section-counter__content">
-                <div>
-                    <h1 className="title-azul cliente-counter"></h1>
+                <div >
+                    <h1 className={`title-azul ${inView ? 'cliente-counter' : ''}`}></h1>
                     <p className="text-azul">Clientes</p>
                     <div className="section-counter__content__text">
-                        <h1 className="title-azul test-counter"></h1>
+                        <h1 className={`title-azul ${inView ? "test-counter" : ''}`}></h1>
                         <h1 className="title-azul">K</h1>
                     </div>
                     <p className="text-azul">Evaluaciones realizadas</p>
                     <div className="section-counter__content__text">
-                        <h1 className="title-azul users-counter"></h1>
+                        <h1 className={`title-azul ${inView ? "users-counter" : ''}`}></h1>
                         <h1 className="title-azul">M</h1>
                     </div>
                     <p className="text-azul">Servicios satisfechos</p>
