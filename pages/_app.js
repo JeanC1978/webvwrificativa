ac64c7d2252423e4b3bed6e734e28b3fbb4b46f8
Juan Silupu <juansilupumaza@gmail.com>
2021-08-04 12:36:57 -0500

    fix: sass text_solutions_evalua

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,6 @@
 import '../styles/globals.scss';
 import '../styles/footer.scss';
 import '../styles/App.scss';
-import '../styles/card.scss';
-import '../styles/slider_cliente.scss';
-import '../styles/slider_logos.scss';
-import '../styles/textsolutions.scss';
-import '../styles/textsolutionsblue.scss';
-import '../styles/textSolutionseconsult.scss';
-import '../styles/textSolutionsevalua.scss';
-import '../styles/cardbennefits.scss';
 
 // import 'bootstrap/dist/css/bootstrap.min.css'
 
